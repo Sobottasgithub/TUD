87b6ef48538df20e64989b410104ad07c92d21b1
Sobottasgithub <patrick.schulze.ofp@gmail.com>
2026-06-03 19:02:42 +0200

    update: implement base functions

--- lib/tud/include/networking.h
+++ lib/tud/include/networking.h
@@ -11,6 +11,10 @@ class Networking
     public:
         std::vector<std::string> getDiscoveredAdresses();
         void removeDiscoveredAddress(std::string address);
+
+        std::string getLocalIpAddress(std::string interface);
+        std::string getBroadcastIpAddress();
+        bool isValidIpV4(std::string &ipString);
         
     protected:
         int sendMessageTo(int socket, const sockaddr_in& broadcast, std::string payload);
