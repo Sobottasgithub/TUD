0264562fffcacbdebd097ac0be18fee6205e8dd9
Patrick Schulze <patrick.schulze.ofp@gmail.com>
2026-09-17 09:41:56 +0200

    update: add argv's

--- test/peer/main.cpp
+++ test/peer/main.cpp
@@ -1,7 +1,47 @@
 #include "peer_discovery.h"
 
+#include <stdexcept>
+#include <string>
+#include <memory>
+#include <optional>
+
+#include <tablog_registry.h>
+#include <tablog.h>
+
 using namespace tud;
 
-int main() {
-  tud::PeerDiscovery peerDiscovery;
+std::string getArg(int argc, char *argv[], std::string argumentName) {
+  for(int index = 0; index < argc; index++) {
+    if (std::string(argv[index]).rfind(argumentName, 0) == 0) {
+      std::string argument = argv[index+1];
+      return argument;
+    }
+  }
+  std::string errorMessage = "Unable to find " + argumentName;
+  throw std::invalid_argument(errorMessage);
 }
+
+int getPort(int argc, char *argv[], std::string argumentName, std::string alternativArgumentName) {
+  try {
+    std::string stringPort = getArg(argc, argv, argumentName);
+    // WARNING: it is not checked if stringPort is an int because it is supposed to throw an error
+    // when a faulty port is provided!
+    return std::stoi(stringPort);
+  } catch (const std::invalid_argument& invalidArgument) {
+    std::string stringPort = getArg(argc, argv, alternativArgumentName);
+    // WARNING: it is not checked if stringPort is an int because it is supposed to throw an error
+    // when a faulty port is provided!
+    return std::stoi(stringPort);
+  }
+}
+
+int main(int argc, char *argv[]) {  
+  std::string interface = getArg(argc, argv, "--interface");
+
+  int broadcastPort = getPort(argc, argv, "--broadcastPort", "-bP");
+  int responsePort = getPort(argc, argv, "--responsePort", "-rsP");
+  int registerPort = getPort(argc, argv, "--registerPort", "-rgP");
+  
+  tud::PeerDiscovery peerDiscovery(interface, broadcastPort, responsePort, registerPort, std::nullopt);
+}
+
